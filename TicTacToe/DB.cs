c4dee24c97f30e4d4d3301804f7890b0928855a8
Exsysr <158147851+Exsysr@users.noreply.github.com>
2024-06-04 19:33:22 +0200

    Uploaded

---
+++ TicTacToe/DB.cs
@@ -0,0 +1,224 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class DB
+    {
+        // Connection string till databasen
+        string connectionString = "datasource=localhost;port=3306;username=root;password=;database=tictactoe";
+
+        public List<Turn> GetTurn() // Hämtar senaste dragen
+        {
+            // Skapar en lista för att lagra dragen
+            List<Turn> turns = new List<Turn>();
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                // sql fråga som hämtar de senaste dragen
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM `playermove`\r\nWHERE ID = (\r\nSELECT MAX(ID) FROM `playermove`)", conn);
+                // Läser av svaret från databasen
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Turn turn = new Turn();
+                        turn.ID = reader.GetInt32(0);
+                        turn.playerone = reader.GetInt32(1);
+                        turn.playertwo = reader.GetInt32(2);
+                        turns.Add(turn);
+                    }
+                }
+            }
+            return turns;
+        }
+
+        public List<Turn> SetTurn(Turn turn) // Sätter drag i databasen
+        {
+            // Skapar en anslutning till databasen
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            // sql fråga som sätter draget i databasen
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO playermove (`PlayerOne`, `PlayerTwo`) VALUES (@playerOne, @PlayerTwo)", conn);
+
+            // Variabler som hämtas från programmet och sätts i databasen
+            cmd.Parameters.AddWithValue("@PlayerOne", turn.playerone);
+            cmd.Parameters.AddWithValue("@PlayerTwo", turn.playertwo);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return GetTurn();
+        }
+
+        public List<Move> MoveFirst() // Sätter in data om vem som spelas först
+        {
+            List<Move> moves = new List<Move>();
+            GetTurn();
+            List<Turn> turns = GetTurn();
+            Turn trns = new Turn();
+            trns.playerone = turns[0].playerone;
+            trns.playertwo = turns[0].playertwo;
+            int p1 = trns.playerone;
+            int p2 = trns.playertwo;
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            // om det blir lika sätts ett nytt värde
+            if (p1 == p2)
+            {
+                Random random = new Random();
+                p1 = random.Next(1, 6);
+                p2 = random.Next(1, 6);
+                // sql fråga som sätter in i databasen
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO `playermove` (`PlayerOne`, `PlayerTwo`) VALUES (@p1, @p2)", conn);
+                // variabler från programmet som sätts i databasen
+                cmd.Parameters.AddWithValue("@p1", p1);
+                cmd.Parameters.AddWithValue("@p2", p2);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return MoveFirst();
+            }
+            else if (p1 > p2)
+            {
+                // Om spelare 1 spelar först sätts det in i databasen
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO `moves` (`P1`, `P2`) VALUES (1, 0)", conn);
+                cmd.ExecuteNonQuery();
+            }
+            else if (p1 < p2)
+            {
+                // Om spelare 2 spelar först sätts det in i databasen
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO `moves` (`P1`, `P2`) VALUES (0, 1)", conn);
+                cmd.ExecuteNonQuery();
+            }
+
+            conn.Close();
+            return moves;
+        }
+
+        public List<Move> GetFirstMove() // Hämtar vem som spelar först
+        {
+            List<Move> moves = new List<Move>();
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            // sql fråga som hämtar vem som spelar först
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM `moves` WHERE ID = (SELECT MAX(ID) FROM `moves`)", conn);
+            // Läser av svaret från databasen
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Move move = new Move();
+                    move.ID = reader.GetInt32(0);
+                    move.move1 = reader.GetBoolean(1);
+                    move.move2 = reader.GetBoolean(2);
+                    moves.Add(move);
+                }
+            }
+            conn.Close();
+            return moves;
+        }
+
+        public List<Player> SetPlayerMove(int x, int y) // Sätter in spelarens drag i databasen
+        {
+            List<Player> players = new List<Player>();
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            // sql fråga som sätter in spelarens drag i databasen
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO `playerone` (`X`, `Y`) VALUES (@x, @y)", conn);
+            // variabler från programmet som sätts i databasen
+            cmd.Parameters.AddWithValue("@x", x);
+            cmd.Parameters.AddWithValue("@y", y);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return players;
+        }
+
+        public List<Player> GetPlayerMove() // Hämtar spelarens drag från databasen
+        {
+            List<Player> players = new List<Player>();
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            // sql fråga som hämtar spelarens senaste drag från databasen
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM `playertwo` WHERE ID = (SELECT MAX(ID) FROM `playertwo`)", conn);
+            // Läser av svaret från databasen
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Player player = new Player();
+                    player.ID = reader.GetInt32(0);
+                    player.X = reader.GetInt32(1);
+                    player.Y = reader.GetInt32(2);
+                    players.Add(player);
+                }
+            }
+            conn.Close();
+            return players;
+        }
+
+        public List<PlayerTurn> GetPlayerTurn() // Hämtar vem som ska spela
+        {
+            List<PlayerTurn> playerTurns = new List<PlayerTurn>();
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            // sql fråga som hämtar vem som ska spela
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM `turn` WHERE ID = (SELECT MAX(ID) FROM `turn`)", conn);
+            // Läser av svaret från databasen
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    PlayerTurn playerTurn = new PlayerTurn();
+                    playerTurn.ID = reader.GetInt32(0);
+                    playerTurn.playerone = reader.GetBoolean(1);
+                    playerTurn.playertwo = reader.GetBoolean(2);
+                    playerTurns.Add(playerTurn);
+                }
+            }
+            conn.Close();
+            return playerTurns;
+        }
+
+        public List<PlayerTurn> SetPlayerTurn(bool a, bool b) // Sätter vem som ska spela
+        {
+            List<PlayerTurn> playerTurns = new List<PlayerTurn>();
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            // sql fråga som sätter vem som ska spela
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO `turn` (`P1T`, `P2T`) VALUES (@playerOne, @PlayerTwo)", conn);
+            // variabler från programmet som sätts i databasen
+            cmd.Parameters.AddWithValue("@PlayerOne", a);
+            cmd.Parameters.AddWithValue("@PlayerTwo", b);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return playerTurns;
+        }
+
+        public void Reset() // Nollställer databasen
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("TRUNCATE TABLE `playermove`", conn);
+            cmd.ExecuteNonQuery();
+            MySqlCommand cmd1 = new MySqlCommand("TRUNCATE TABLE `moves`", conn);
+            cmd1.ExecuteNonQuery();
+            MySqlCommand cmd2 = new MySqlCommand("TRUNCATE TABLE `playerone`", conn);
+            cmd2.ExecuteNonQuery();
+            MySqlCommand cmd3 = new MySqlCommand("TRUNCATE TABLE `playertwo`", conn);
+            cmd3.ExecuteNonQuery();
+            MySqlCommand cmd4 = new MySqlCommand("TRUNCATE TABLE `turn`", conn);
+            cmd4.ExecuteNonQuery();
+            // Lägger till att det inte är någons tur
+            MySqlCommand cmd5 = new MySqlCommand("INSERT INTO `turn`(`ID`, `P1T`, `P2T`) VALUES ('1','0','0')", conn);
+            cmd5.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,357 @@
+﻿//PLAYER ONE
+using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        // globala variabler
+        static int[,] board = new int[3, 3];
+        static string[,] plane = new string[3, 3];
+        static int p1;
+        static int p2;
+        static bool player1;
+        static bool player2;
+        static bool Win = false;
+        static string? winner;
+
+        static void Main()
+        {
+            DB db = new DB(); // skapar en ny instans av klassen DB som länkar till databasen
+            Turns();
+            db.MoveFirst();
+            List<Turn> turns = db.GetTurn();
+            // sätter in värden från databasen i globala variabler
+            p1 = turns[0].playerone;
+            p2 = turns[0].playertwo;
+
+            Console.WriteLine("Welcome too TickTacToe!!!");
+            Thread.Sleep(500);
+            Console.WriteLine("Player 1 is X and Player 2 is O!");
+            Thread.Sleep(500);
+            Console.WriteLine("Lest role a dice to see who goes first!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Player 1 rolled: " + p1);
+            Thread.Sleep(500);
+            Console.WriteLine("And...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Player 2 rolled: " + p2);
+            Thread.Sleep(500);
+
+            FirstTurn();
+            Playing();
+        } // introducerar spelet och kör igång spelet
+        static void Playing()
+        {
+            // Körs så länge ingen har vunnit
+            DB db = new DB();
+            while (!Win)
+            {
+                Turn();
+                Console.Clear();
+                CheckForWin();
+            }
+        } // Kör spelet
+        static void Turns()
+        {
+            DB db = new DB();
+            Random random = new Random();
+            // slumpar fram ett tal mellan 1 och 6
+            int o = random.Next(1, 6); 
+            int t = random.Next(1, 6);
+            // sätter in värdena i databasen
+            db.SetTurn(new Turn() { playerone = o, playertwo = t }); 
+        } // en metod som slumpar fram två tal och sätter in dem i databasen
+        static void DrawBoard()
+        {
+            // Konverterar siffror till X, O eller " "
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        plane[i, j] = " ";
+                    }
+                    else if (board[i, j] == 1)
+                    {
+                        plane[i, j] = "X";
+                    }
+                    else if (board[i, j] == 2)
+                    {
+                        plane[i, j] = "O";
+                    }
+                }
+            }
+            // Skriver ut spelplanen
+            Console.WriteLine("   |  1. |  2. |  3. | ");
+            Console.WriteLine("___|_____|_____|_____|");
+            Console.WriteLine("   |     |     |     | ");
+            Console.WriteLine("1. |  {0}  |  {1}  |  {2}  |", plane[0, 0], plane[0, 1], plane[0, 2]);
+            Console.WriteLine("___|_____|_____|_____| ");
+            Console.WriteLine("   |     |     |     | ");
+            Console.WriteLine("2. |  {0}  |  {1}  |  {2}  |", plane[1, 0], plane[1, 1], plane[1, 2]);
+            Console.WriteLine("___|_____|_____|_____| ");
+            Console.WriteLine("   |     |     |     | ");
+            Console.WriteLine("3. |  {0}  |  {1}  |  {2}  |", plane[2, 0], plane[2, 1], plane[2, 2]);
+            Console.WriteLine("___|_____|_____|_____| ");
+            Console.WriteLine("Player 1 is X and Player 2 is O! \n");
+        } // skriver ut spelplanen
+        static void CheckForWin()
+        {
+            DB db = new DB();
+
+            // Horisontell vinst
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == 1 && board[i, 1] == 1 && board[i, 2] == 1)
+                {
+                    winner = "Player 1 wins!";
+                    Win = true;
+                    db.Reset();
+                }
+                else if (board[i, 0] == 2 && board[i, 1] == 2 && board[i, 2] == 2)
+                {
+                    winner = "Player 2 wins!";
+                    Win = true;
+                    db.Reset();
+                }
+            }
+
+            // Vertical vinst
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == 1 && board[1, i] == 1 && board[2, i] == 1)
+                {
+                    winner = "Player 1 wins!";
+                    Win = true;
+                    db.Reset();
+                }
+                else if (board[0, i] == 2 && board[1, i] == 2 && board[2, i] == 2)
+                {
+                    winner = "Player 2 wins!";
+                    Win = true;
+                    db.Reset();
+                }
+            }
+
+            // Diagonal vinst
+            if (board[0, 0] == 1 && board[1, 1] == 1 && board[2, 2] == 1)
+            {
+                winner = "Player 1 wins!";
+                Win = true;
+                db.Reset();
+            }
+            else if (board[0, 0] == 2 && board[1, 1] == 2 && board[2, 2] == 2)
+            {
+                winner = "Player 2 wins!";
+                Win = true;
+                db.Reset();
+            }
+            if (board[0, 2] == 1 && board[1, 1] == 1 && board[2, 0] == 1)
+            {
+                winner = "Player 1 wins!";
+                Win = true;
+                db.Reset();
+            }
+            else if (board[0, 2] == 2 && board[1, 1] == 2 && board[2, 0] == 2)
+            {
+                winner = "Player 2 wins!";
+                Win = true;
+                db.Reset();
+            }
+
+            // Oavgjort
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] != 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            if (count == 9)
+            {
+                winner = "The game is a draw!";
+                Win = true;
+                db.Reset(); 
+            }
+
+            // Slut på spelet
+            if (Win)
+            {
+                Console.Clear();
+                player1 = true;
+                DrawBoard();
+                Console.WriteLine(winner);
+                Console.WriteLine("Game Over!");
+                Environment.Exit(0);
+            }
+
+        } // Kollar om någon har vunnit
+        static void PlayerTurn()
+        {
+            DB dB = new DB();
+
+            try
+            {
+                // Hämtar andra spelarens tur från databasen
+                List<Player> players = dB.GetPlayerMove();
+                Player player = new Player();
+                player.X = players[0].X;
+                player.Y = players[0].Y;
+                if (board[player.X, player.Y] == 0)
+                {
+                    board[player.X, player.Y] = 2;
+                }
+            }
+            catch
+            {
+            }
+
+            DrawBoard();
+            int x = 0;
+            int y = 0;
+            Console.Write("Enter the row number: ");
+            try
+            {
+                x = int.Parse(Console.ReadLine()!) - 1;
+                // Ser till att spelaren spelar inom spelplanen
+                if (x > 2 || x < 0) 
+                {
+                    Console.WriteLine(x + " is not a valid row number. Please enter a number between 0 and 3!");
+                    Console.Clear();
+                    PlayerTurn();
+                }
+            }
+            catch
+            {
+                // Om spelaren inte skriver in ett nummer
+                Console.WriteLine("Please enter a valid number!");
+                Console.Clear();
+                PlayerTurn();
+            }
+
+            Console.Write("Enter the column number: ");
+            try
+            {
+                y = int.Parse(Console.ReadLine()!) - 1;
+                // Ser till att spelaren spelar inom spelplanen
+                if (y > 2 || y < 0)
+                {
+                    Console.WriteLine(y + " is not a valid column number. Please enter a number between 0 and 3!");
+                    Console.Clear();
+                    PlayerTurn();
+                }
+            }
+            catch
+            {
+                // Om spelaren inte skriver in ett nummer
+                Console.WriteLine("Please enter a valid number!");
+                Console.Clear();
+                PlayerTurn();
+            }
+
+            // Ser till att spelaren inte kan skriva över en annan spelares drag
+            if (board[x, y] == 0)
+            {
+                board[x, y] = 1;
+            }
+            else
+            {
+                // Om spelaren spelar på en redan tagen plats får de köra om
+                Console.WriteLine($"The spot ({x}, {y}) is already taken!");
+                PlayerTurn();
+            }
+
+            // Skriver in spelarens drag i databasen
+            dB.SetPlayerMove(x, y);
+            dB.SetPlayerTurn(false, true);
+
+            Playing();
+        } // Spelarens tur
+        static void FirstTurn()
+        {
+            DB db = new DB();
+            // Hämtar vem som börjar spelet från databasen
+            List<Move> moves = db.GetFirstMove();
+            player1 = moves[0].move1;
+            if (player1)
+            {
+                // Om spelaren börjar
+                Console.Clear();
+                Console.WriteLine("Its your turn :) \n");
+                Console.WriteLine(moves[0].move1);
+                PlayerTurn();
+            }
+            else
+            {
+                // Om andra spelaren börjar
+                Console.Clear();
+                Console.WriteLine("Its not your turn :( \n");
+                Console.WriteLine(moves[0].move1);
+                DrawBoard();
+                Turn();
+            }
+        } // Vem som börjar spelet
+        static void Turn() 
+        {
+            DB db = new DB();
+
+            // försöker hämta vems tur det är
+            try
+            {
+                // Hämtar vems tur det är från databasen
+                List<PlayerTurn> playerTurns = db.GetPlayerTurn();
+                player1 = playerTurns[0].playerone;
+                if (player1)
+                {
+                    // Om det är spelarens tur
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("Its your turn :) \n");
+                    PlayerTurn();
+                }
+                else
+                {
+                    // Om det inte är spelarens tur
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("Its not your turn :( \n");
+                    DrawBoard();
+                    while (!player1)
+                    {
+                        try
+                        {
+                            // försöker hämta andra spelarens drag
+                            List<Player> players = db.GetPlayerMove();
+                            Player player = new Player();
+                            player.X = players[0].X;
+                            player.Y = players[0].Y;
+                            if (board[player.X, player.Y] == 0)
+                            {
+                                board[player.X, player.Y] = 2;
+                            }
+                        }
+                        catch
+                        {
+                        }
+                        // Hämtar vems tur det är
+                        List<PlayerTurn> playerTurns1 = db.GetPlayerTurn();
+                        player1 = playerTurns1[0].playerone;
+                        CheckForWin(); // kollar om någon har vunnit
+                    }
+                }
+            }
+            catch
+            {
+                // Om det inte finns något i databasen
+                player1 = false;
+                Turn();
+            }
+        } // Vem spelar
+    }
+}
